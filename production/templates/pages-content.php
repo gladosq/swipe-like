fb97db7489ab7cc4c31bd8b40bb0048b9845dae1
gladosq <gladosq@gmail.com>
2022-07-16 15:46:10 +0400

    deleted assets

--- production/templates/pages-content.php
+++ production/templates/pages-content.php
@@ -6,6 +6,7 @@
       <li class="pages__item"><a href="/second-page" target="_blank">Страница №2</a></li>
       <li class="pages__item"><a href="/third-page" target="_blank">Страница №3</a></li>
     </ul>
+    <p>GitHub: <a href="https://github.com/gladosq/swipe-like">https://github.com/gladosq/swipe-like</a></p>
     <p>Исполнитель: Шишкин Никита Юрьевич</p>
     <p>Telegram/VK: <a href="https://t.me/gladosq">@gladosq</a>/<a href="https://vk.com/gladosq">@gladosq</a></p>
     <p>Email: <a href="mailto:gladosq@gmail.com">gladosq@gmail.com</a></p>
